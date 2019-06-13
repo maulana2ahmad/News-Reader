575ca61e6dd7f60a708f23dbeb7114d34ac096e8
_REDUDANT <maulana2ahmad@gmail.com>
2019-06-13 11:19:20 +0700

    add articleactivity and move settingapi to folder

--- app/src/main/java/com/redudant/newsreader/MainActivity.java
+++ app/src/main/java/com/redudant/newsreader/MainActivity.java
@@ -1,5 +1,6 @@
 package com.redudant.newsreader;
 
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteStatement;
@@ -7,8 +8,12 @@ import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import com.redudant.newsreader.SettingApi.SettingApi;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -55,7 +60,21 @@ public class MainActivity extends AppCompatActivity
 
         listView.setAdapter(arrayAdapter);
 
-        //creat
+        //onClick listview
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                Intent intent = new Intent(getApplicationContext(), ArticleActivity.class);
+                intent.putExtra("content", content.get(position));
+
+                startActivity(intent);
+
+            }
+        });
+        //end onClick listview
+
+        //creat database
         articlesDB = this.openOrCreateDatabase("Articles", MODE_PRIVATE, null);
 
         //create table
--- app/src/main/java/com/redudant/newsreader/SettingApi/SettingApi.java
+++ app/src/main/java/com/redudant/newsreader/SettingApi/SettingApi.java
@@ -1,4 +1,4 @@
-package com.redudant.newsreader;
+package com.redudant.newsreader.SettingApi;
 
 public class SettingApi {
 
@@ -9,6 +9,8 @@ public class SettingApi {
 //    public static final String IP_ITEM =
 //            "https://hacker-news.firebaseio.com/v0/item/" + MainActivity.articleId + ".json?print=pretty";
 
+    //menggantin ID = 8863., dengan article yang kita inginkan
+    //https://hacker-news.firebaseio.com/v0/item/8863.json?print=pretty
     public static final String IP_ITEM =
           "https://hacker-news.firebaseio.com/v0/item/";
 }
